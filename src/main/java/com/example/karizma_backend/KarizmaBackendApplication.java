f6264696208cb5fce6aa07b7edf40c979a3083c8
MOURID Zakaria <zakariamourid142@gmail.com>
2023-11-25 09:23:38 +0100

    Initial commit

---
+++ src/main/java/com/example/karizma_backend/KarizmaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.karizma_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarizmaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KarizmaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/karizma_backend/KarizmaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.karizma_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarizmaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
